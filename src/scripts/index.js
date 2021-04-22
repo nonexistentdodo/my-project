53680752f9d3f3fcb983bb4693b6bfa5b1f3f8a6
nonexistentdodo <nonexistentdodo@protonmail.com>
2021-04-22 16:22:19 +0200

    larama

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,43 @@
 // Any code you will write for your website (in the future!) should go here
+'use strict'
+
+const formV = document.querySelector('form')
+const textV = document.querySelector('.form-control')
+const divV = document.querySelector('.list')
+const bntV = document.querySelector('.btn')
+
+
+formV.addEventListener('submit', (evt) => {
+    evt.preventDefault()
+    let MyText = textV.value 
+    console.log(MyText)
+
+    let ParagraphV = document.createElement('p');
+    let buttonV = document.createElement('span');
+
+    buttonV.innerText = 'X';
+
+    ParagraphV.innerText = MyText;
+
+    ParagraphV.appendChild(buttonV);
+
+    divV.appendChild(ParagraphV);
+
+    textV.value = '';
+
+buttonV.addEventListener('click',(evt) => {
+let ThingToRemove = evt.target.parentElement
+
+divV.removeChild(ThingToRemove)
+
+}
+)
+
+
+});
+
+
+
+
+
+
